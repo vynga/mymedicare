4f948e0d0b4d67b8a84fe6bce29d40025efb5f01
Arun Viswanathan <arunplr@gmail.com>
2020-09-13 16:35:19 -0400

    first commit

---
+++ src/main/java/com/vynga/insurance/mymedicare/MymedicareApplication.java
@@ -0,0 +1,13 @@
+package com.vynga.insurance.mymedicare;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MymedicareApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MymedicareApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/vynga/insurance/mymedicare/MymedicareApplicationTests.java
@@ -0,0 +1,13 @@
+package com.vynga.insurance.mymedicare;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MymedicareApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
